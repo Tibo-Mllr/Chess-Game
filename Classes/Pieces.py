3cb66308e03b80b8dc4abe6d1a3c30524d4c2da1
Thibault Muller <thibault.muller@student-cs.fr>
2021-11-15 15:08:40 +0100

    Name

--- Classes/Pieces.py
+++ Classes/Pieces.py
@@ -9,6 +9,7 @@ class Pion:
 
         self.Color = color
         self.Moved = False
+        self.name = 'pion'
 
     def move(self, c):
         # Peut bouger de 2 SI ET SEULEMENT SI il n'a pas bougé
@@ -38,7 +39,7 @@ class Pion:
     def change(self, choice):
         if self.Y_Pos in [7, 0]:
             # ATTENTION : Concordance à vérifier
-            self = choice(self.Color, 'g', True, self.Pos_X, self.Y_pos)
+            self = choice(self.Color, 'g', True, self.Pos_X, self.Pos_Y)
 
 
 class Roi:
@@ -54,6 +55,7 @@ class Roi:
         self.Moved = False
         self.Checked = False
         self.Color = color
+        self.name = 'roi'
 
     def move(self, d):
 
@@ -86,151 +88,167 @@ class Roi:
                 self.Pos_X += 2
             else:
                 self.Pos_X -= 2
+
+
 class Tour:
-    
-    def __init__(self,cote,color,Change = False, X=3, Y=3): #cote = d ou g, color = White ou Black
-        if not Change :
-            if cote == 'g' :   
-                self.Pos_X=0
-            if cote == 'd' :
-                self.Pos_X=7
+
+    # cote = d ou g, color = White ou Black
+    def __init__(self, cote, color, Change=False, X=3, Y=3):
+        if not Change:
+            if cote == 'g':
+                self.Pos_X = 0
+            if cote == 'd':
+                self.Pos_X = 7
             if color == 'White':
-                self.Pos_Y=0
+                self.Pos_Y = 0
             if color == 'Black':
-                self.Pos_Y=7
-        if Change : #Au cas où ce soit une transformation de pion
+                self.Pos_Y = 7
+        if Change:  # Au cas où ce soit une transformation de pion
             self.Pos_X = X
             self.Pos_Y = Y
             self.Color = color
-        self.Color=color
-        self.Moved=False
+        self.Color = color
+        self.Moved = False
+        self.name = 'tour'
 
     def petit_rock(self):
-        if self.Moved==False : #and Condition roi pas mis en échec et pas de pièce sur le chemin
-            self.Pos_X=5
-            self.Moved=True
-    
+        if self.Moved == False:  # and Condition roi pas mis en échec et pas de pièce sur le chemin
+            self.Pos_X = 5
+            self.Moved = True
+
     def grand_rock(self):
-        if self.Moved==False : #and Condition roi pas mis en échec et pas de pièce sur le chemin
-            self.Pos_X=3
-            self.Moved=True
-    
-    def move(self,valeur,direction): #longueur dans [|-7;7|], direction = horizontale ou verticale
-        if direction=='horizontale' :#and pas de pièce en chemin
-            self.Pos_X+=valeur
-        elif direction=='verticale': #and pas de pièce en chemin
-            self.Pos_Y+=valeur 
-        self.Moved=True
-    
+        if self.Moved == False:  # and Condition roi pas mis en échec et pas de pièce sur le chemin
+            self.Pos_X = 3
+            self.Moved = True
+
+    # longueur dans [|-7;7|], direction = horizontale ou verticale
+    def move(self, valeur, direction):
+        if direction == 'horizontale':  # and pas de pièce en chemin
+            self.Pos_X += valeur
+        elif direction == 'verticale':  # and pas de pièce en chemin
+            self.Pos_Y += valeur
+        self.Moved = True
+
+
 class Dame:
-   
-    def __init__(self,cote,color,Change = False, X=3, Y=3): #color = White ou Black et on met l'argument cote pour homogénéiser entre les différentes fonctions 
-        if not Change :
+
+    # color = White ou Black et on met l'argument cote pour homogénéiser entre les différentes fonctions
+    def __init__(self, cote, color, Change=False, X=3, Y=3):
+        if not Change:
             if color == 'White':
-                self.Pos_Y=0
+                self.Pos_Y = 0
             else:
-                self.Pos_Y=7
-            self.Pos_X=3
-        if Change : #Au cas où ce soit une transformation de pion
+                self.Pos_Y = 7
+            self.Pos_X = 3
+        if Change:  # Au cas où ce soit une transformation de pion
             self.Pos_X = X
             self.Pos_Y = Y
         self.Color = color
-    def move_normal(self,valeur,direction): #longueur dans [|-7;7|], direction = horizontale ou verticale
-        if direction=='horizontale' :#and pas de pièce en chemin
-            self.Pos_X+=valeur
-        elif direction=='verticale': #and pas de pièce en chemin
-            self.Pos_Y+=valeur
-    
-    def move_diagonal(self,valeur,direction): #direction =h_d ou h_g ou b_d ou b_g
-        if direction=='h_d': #and pas de pièce en chemin
-            self.Pos_X+=valeur
-            self.Pos_Y+=valeur
-        
-        elif direction=='h_g': #and pas de pièce en chemin
-            self.Pos_X-=valeur
-            self.Pos_Y+=valeur
-        
-        elif direction=='b_d': #and pas de pièce en chemin
-            self.Pos_X+=valeur
-            self.Pos_Y-=valeur
-        elif direction=='b_g': #and pas de pièce en chemin
-            self.Pos_X-=valeur
-            self.Pos_Y-=valeur
+        self.name = 'dame'
 
-        
-class Fou :
-    
-    def __init__(self,cote,color,Change = False, X=3, Y=3): #cote = d ou g, color = White ou Black
-        if not Change :
-            if cote == 'g' :   
-                self.Pos_X=2
-            if cote == 'd' :
-                self.Pos_X=5
+    # longueur dans [|-7;7|], direction = horizontale ou verticale
+    def move_normal(self, valeur, direction):
+        if direction == 'horizontale':  # and pas de pièce en chemin
+            self.Pos_X += valeur
+        elif direction == 'verticale':  # and pas de pièce en chemin
+            self.Pos_Y += valeur
+
+    def move_diagonal(self, valeur, direction):  # direction =h_d ou h_g ou b_d ou b_g
+        if direction == 'h_d':  # and pas de pièce en chemin
+            self.Pos_X += valeur
+            self.Pos_Y += valeur
+
+        elif direction == 'h_g':  # and pas de pièce en chemin
+            self.Pos_X -= valeur
+            self.Pos_Y += valeur
+
+        elif direction == 'b_d':  # and pas de pièce en chemin
+            self.Pos_X += valeur
+            self.Pos_Y -= valeur
+        elif direction == 'b_g':  # and pas de pièce en chemin
+            self.Pos_X -= valeur
+            self.Pos_Y -= valeur
+
+
+class Fou:
+
+    # cote = d ou g, color = White ou Black
+    def __init__(self, cote, color, Change=False, X=3, Y=3):
+        if not Change:
+            if cote == 'g':
+                self.Pos_X = 2
+            if cote == 'd':
+                self.Pos_X = 5
             if color == 'White':
-                self.Pos_Y=0
+                self.Pos_Y = 0
             if color == 'Black':
-                self.Pos_Y=7
-        if Change : #Au cas où ce soit une transformation de pion
+                self.Pos_Y = 7
+        if Change:  # Au cas où ce soit une transformation de pion
             self.Pos_X = X
             self.Pos_Y = Y
-        self.Color=color
-    
-    def move(self,valeur,direction): #direction =h_d ou h_g ou b_d ou b_g
-        if direction=='h_d': #and pas de pièce en chemin
-            self.Pos_X+=valeur
-            self.Pos_Y+=valeur
-        
-        elif direction=='h_g': #and pas de pièce en chemin
-            self.Pos_X-=valeur
-            self.Pos_Y+=valeur
-        
-        elif direction=='b_d': #and pas de pièce en chemin
-            self.Pos_X+=valeur
-            self.Pos_Y-=valeur
-        elif direction=='b_g': #and pas de pièce en chemin
-            self.Pos_X-=valeur
-            self.Pos_Y-=valeur
+        self.Color = color
+        self.name = 'fou'
+
+    def move(self, valeur, direction):  # direction =h_d ou h_g ou b_d ou b_g
+        if direction == 'h_d':  # and pas de pièce en chemin
+            self.Pos_X += valeur
+            self.Pos_Y += valeur
+
+        elif direction == 'h_g':  # and pas de pièce en chemin
+            self.Pos_X -= valeur
+            self.Pos_Y += valeur
+
+        elif direction == 'b_d':  # and pas de pièce en chemin
+            self.Pos_X += valeur
+            self.Pos_Y -= valeur
+        elif direction == 'b_g':  # and pas de pièce en chemin
+            self.Pos_X -= valeur
+            self.Pos_Y -= valeur
+
 
 class Cavalier:
 
-    def __init__(self,cote,color,Change = False, X=3, Y=3): #cote = d ou g, color = White ou Black
-        if not Change :
-            if cote == 'g' :   
-                self.Pos_X=1
-            if cote == 'd' :
-                self.Pos_X=6
+    # cote = d ou g, color = White ou Black
+    def __init__(self, cote, color, Change=False, X=3, Y=3):
+        if not Change:
+            if cote == 'g':
+                self.Pos_X = 1
+            if cote == 'd':
+                self.Pos_X = 6
             if color == 'White':
-                self.Pos_Y=0
+                self.Pos_Y = 0
             if color == 'Black':
-                self.Pos_Y=7
-        if Change : #Au cas où ce soit une transformation de pion
+                self.Pos_Y = 7
+        if Change:  # Au cas où ce soit une transformation de pion
             self.Pos_X = X
             self.Pos_Y = Y
-        self.Color=color
-    
-    def move(self,valeur,direction): #direction =h_d_d ou h_g_g ou h_h_g ou h_h_d ou b_d_d ou b_g_g ou b_b_g ou b_b_d
+        self.Color = color
+        self.name = 'cavalier'
+
+    # direction =h_d_d ou h_g_g ou h_h_g ou h_h_d ou b_d_d ou b_g_g ou b_b_g ou b_b_d
+    def move(self, valeur, direction):
 
         if direction == 'h_d_d':
-            self.Pos_X+=2
-            self.Pos_Y+=1
+            self.Pos_X += 2
+            self.Pos_Y += 1
         elif direction == 'h_g_g':
-            self.Pos_X-=2
-            self.Pos_Y+=1
+            self.Pos_X -= 2
+            self.Pos_Y += 1
         elif direction == 'h_h_g':
-            self.Pos_X-=1
-            self.Pos_Y+=2
+            self.Pos_X -= 1
+            self.Pos_Y += 2
         elif direction == 'h_h_d':
-            self.Pos_X+=1
-            self.Pos_Y+=2
+            self.Pos_X += 1
+            self.Pos_Y += 2
         elif direction == 'b_d_d':
-            self.Pos_X+=2
-            self.Pos_Y-=1
+            self.Pos_X += 2
+            self.Pos_Y -= 1
         elif direction == 'b_g_g':
-            self.Pos_X-=2
-            self.Pos_Y-=1
+            self.Pos_X -= 2
+            self.Pos_Y -= 1
         elif direction == 'b_b_g':
-            self.Pos_X-=1
-            self.Pos_Y-=2
+            self.Pos_X -= 1
+            self.Pos_Y -= 2
         elif direction == 'b_b_d':
-            self.Pos_X+=1
-            self.Pos_Y-=2
+            self.Pos_X += 1
+            self.Pos_Y -= 2
