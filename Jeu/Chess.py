e0f3e2e1b9d92ba41678633f42b72dd511348ad1
Vacherot Oscar <oscar.vacherot@student-cs.fr>
2021-11-17 15:20:04 +0100

    modification

--- Jeu/Chess.py
+++ Jeu/Chess.py
@@ -1,19 +1,17 @@
 from Move import *
 from Classes.Pieces import *
-from Interface.Graphique import *
-
 
 def egalite(roi,plateau):
-    if mvt_possible_roi(roi,plateau)==[] and not roi_en_echec(roi,plateau):
+    if mvt_final(roi,plateau)==[] and not roi_en_echec(roi,plateau):
         mvt_possible_autres_pièces=[]
-        for i in [Pion,Tour,Fou,Dame,Cavalier]:
-            mvt_possible_autres_pièces+=mvt_possible_gen(i,plateau)
+        for piece in [Pion,Tour,Fou,Dame,Cavalier]:
+            mvt_possible_autres_pièces+=mvt_possible_gen(piece,plateau)
         if mvt_possible_autres_pièces==[]:
             print  ("it's a draw")
 
 
 def victoire(roi,plateau):
-    if mvt_possible_roi(roi,plateau)==[] and roi_en_echec(roi,plateau):
+    if mvt_final(roi,plateau)==[] and roi_en_echec(roi,plateau):
         couleur_gagnant=["White","Black"]
         couleur_gagnant.remove(roi.Color)
         print(couleur_gagnant[0] + "Win !")
