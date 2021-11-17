d34bc8dee2ce401b9904fcc55142da56375fd62a
Bilal_Kopp <bilal.kopp@student-cs.fr>
2021-11-17 16:52:39 +0100

    qques corrections

--- Jeu/Move.py
+++ Jeu/Move.py
@@ -172,12 +172,12 @@ def mvt_possible_fou(fou, plateau):
             if finbasdroite == False:
                 if case_libre(fou.Pos_X + i, fou.Pos_Y - i, plateau):
                     mvt_possible = mvt_possible + \
-                        [(fou.Pos_X + i, fou.Pos_X - i)]
+                        [(fou.Pos_X + i, fou.Pos_Y - i)]
                 else:
                     finbasdroite = True
-                    if case_color(fou.Pos_X + i, fou.Pos_Y - i, plateau) != 0:
+                    if case_color(fou.Pos_X + i, fou.Pos_Y - i, plateau) != fou.Color:
                         mvt_possible = mvt_possible + \
-                            [(fou.Pos_X + i, fou.Pos_X - i)]
+                            [(fou.Pos_X + i, fou.Pos_Y - i)]
     return mvt_possible
 
 
@@ -293,7 +293,7 @@ def echec_si_mouvement_du_roi(roi, x, y, plateau):  # a revoir
     echec_si_mvt = False
     for piece in newplateau.values():
         if piece != '' and piece != roi:
-            if (x, y) in mvt_possible_gen(piece, plateau):
+            if (x, y) in mvt_possible_gen(piece, newplateau):
                 echec_si_mvt = True
     return echec_si_mvt
 
@@ -360,28 +360,26 @@ def echec_si_mvt(piece, x, y, plateau):
     newplateau[(piece.Pos_X, piece.Pos_Y)] = ''
     newplateau[(x, y)] = piece
 
-    for element in plateau:
-        if plateau[element] != '' and plateau[element].name == 'roi' and plateau[element].Color == 'White':
-            RoiBlanc = plateau[element]
-        if plateau[element] != '' and plateau[element].name == 'roi' and plateau[element].Color == 'Black':
-            RoiNoir = plateau[element]
+    for element in newplateau:
+        if newplateau[element] != '' and newplateau[element].name == 'roi' and newplateau[element].Color == 'White':
+            RoiBlanc = newplateau[element]
+        if newplateau[element] != '' and newplateau[element].name == 'roi' and newplateau[element].Color == 'Black':
+            RoiNoir = newplateau[element]
 
     if piece.Color == 'White':
         echec_blanc = False
         for i in newplateau.values():
             if i != '':
-                if (RoiBlanc.Pos_X, RoiBlanc.Pos_Y) in mvt_possible_gen(i, plateau):
+                if (RoiBlanc.Pos_X, RoiBlanc.Pos_Y) in mvt_possible_gen(i, newplateau):
                     echec_blanc = True
-                    RoiBlanc.Checked = True
         return echec_blanc
 
     if piece.Color == 'Black':
         echec_noir = False
         for i in newplateau.values():
             if i != '':
-                if (RoiNoir.Pos_X, RoiNoir.Pos_Y) in mvt_possible_gen(i, plateau):
+                if (RoiNoir.Pos_X, RoiNoir.Pos_Y) in mvt_possible_gen(i, newplateau):
                     echec_noir = True
-                    RoiNoir.Checked = True
         return echec_noir
 
 
@@ -487,3 +485,6 @@ genre une liste qui donne les coups effectués
 
 en plus ca permettrait de rejouer la partie a partir d'une certaine etape
 '''
+
+
+# roi peut pasmanger la piece qui le met en echec
