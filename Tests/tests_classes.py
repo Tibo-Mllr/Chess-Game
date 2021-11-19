3bf5da73d397ee03cce7eeabe989d96b793bcd4a
Thibault Muller <thibault.muller@student-cs.fr>
2021-11-19 11:36:45 +0100

    Tests V2

--- Tests/tests_classes.py
+++ Tests/tests_classes.py
@@ -3,15 +3,15 @@ from Classes.Pieces import *
 
 def test_Pion():
     Pion1 = Pion(0, 'White')
-    Pion1.move(2)
+    Pion1.move(0, 3)
     assert Pion1.Pos_Y == 3
 
-    Pion1.move(2)
+    Pion1.move(0, 4)
     assert Pion1.Pos_Y == 4
 
     Dame2 = Dame('g', 'White', True, Pion1.Pos_X, Pion1.Pos_Y)
-    Dame2.move_normal(-3, 'verticale')
-    assert (Dame2.Pos_X, Dame2.Pos_Y) == (0, 1)
+    Dame2.move(2, 6)
+    assert (Dame2.Pos_X, Dame2.Pos_Y) == (2, 6)
 
 
 def test_Roi():
@@ -19,13 +19,13 @@ def test_Roi():
     Roi1.roque('d')
     assert (Roi1.Pos_X, Roi1.Pos_Y) == (6, 0)
 
-    Roi1.move('h')
+    Roi1.move(6, 1)
     assert (Roi1.Pos_X, Roi1.Pos_Y) == (6, 1)
 
-    Roi1.move('g_b')
+    Roi1.move(5, 1)
     assert (Roi1.Pos_X, Roi1.Pos_Y) == (5, 1)
 
-    Roi1.move('h_d')
+    Roi1.move(6, 2)
     assert (Roi1.Pos_X, Roi1.Pos_Y) == (6, 2)
 
     Roi1.roque('g')
@@ -37,13 +37,13 @@ def test_Tour():
     Tour1.grand_roque()
     assert(Tour1.Pos_X, Tour1.Pos_Y) == (3, 0)
 
-    Tour1.move(4, 'horizontale')
+    Tour1.move(7, 0)
     assert (Tour1.Pos_X, Tour1.Pos_Y) == (7, 0)
 
-    Tour1.move(5, 'verticale')
+    Tour1.move(7, 5)
     assert (Tour1.Pos_X, Tour1.Pos_Y) == (7, 5)
 
-    Tour1.move(-3, 'verticale')
+    Tour1.move(7, 2)
     assert (Tour1.Pos_X, Tour1.Pos_Y) == (7, 2)
 
     Tour1.petit_roque()
@@ -52,26 +52,26 @@ def test_Tour():
 
 def test_Dame():
     Dame1 = Dame('g', 'White')
-    Dame1.move_normal(4, 'verticale')
+    Dame1.move(3, 4)
     assert (Dame1.Pos_X, Dame1.Pos_Y) == (3, 4)
 
-    Dame1.move_diagonal(-3, 'h_d')
+    Dame1.move(0, 1)
     assert (Dame1.Pos_X, Dame1.Pos_Y) == (0, 1)
 
 
 def test_Fou():
     Fou1 = Fou('g', 'White')
-    Fou1.move(5, 'h_d')
+    Fou1.move(7, 5)
     assert (Fou1.Pos_X, Fou1.Pos_Y) == (7, 5)
 
-    Fou1.move(-1, 'b_d')
+    Fou1.move(6, 6)
     assert (Fou1.Pos_X, Fou1.Pos_Y) == (6, 6)
 
 
 def test_Cavalier():
     Cavalier1 = Cavalier('g', 'White')
-    Cavalier1.move(0, 'h_h_d')
+    Cavalier1.move(2, 2)
     assert (Cavalier1.Pos_X, Cavalier1.Pos_Y) == (2, 2)
 
-    Cavalier1.move(0, 'b_b_g')
+    Cavalier1.move(1, 0)
     assert (Cavalier1.Pos_X, Cavalier1.Pos_Y) == (1, 0)
